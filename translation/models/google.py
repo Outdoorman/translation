1e8f180461d5da2e1d1fc55f559520f3138dcbb5
Outdoorman <pontus.luke@gmail.com>
2017-04-27 10:55:35 +0800

    google.cn post提交更新
    URL
       link: https://translate.google.cn/translate_a/single
    参数
       client:at   ps: 少量样本对比测试翻译效果最佳
       dt:t

--- translation/models/google.py
+++ translation/models/google.py
@@ -2,11 +2,12 @@ import requests
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0', }
-BASE_URL = 'http://translate.google.cn/translate_a/t'
+BASE_URL = 'https://translate.google.cn/translate_a/single'
 params = {
-    'client': 'p',
+    'client': 'gtx',
     'ie': 'UTF-8',
     'oe': 'UTF-8',
+    'dt':'t',
     'tl': None,
     'sl': None,
     'text': None, }
@@ -16,7 +17,7 @@ def google(text, src, dst, proxies):
     params['tl'] = dst
     params['sl'] = src
     r = requests.post(BASE_URL, params, headers = headers, proxies = proxies).json()
-    return r[0] if isinstance(r, list) else r
+    return r[0][0][0] if isinstance(r, list) else r
 
 if __name__ == '__main__':
     print(google('Test', 'auto', 'zh-CN', {'http': 'http://127.0.0.1:1080'}))
